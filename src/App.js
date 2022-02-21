380ae350da1811b9b82ebeaaf4cac74040dd74b9
Txema Clifford <txema.clifford@icloud.com>
2022-02-21 18:03:42 +0000

    First commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import Article from "./components/Article";
+
+function App() {
+    const [articles, setArticles] = useState([]);
+    const [subreddit, setSubreddit] = useState("AthleticClub");
+
+    useEffect(() => {
+        fetch("https://www.reddit.com/r/"+ subreddit + ".json").then(res => {
+            if (res.status != 200) {
+                console.log("Error!!!");
+                return;
+            }
+
+            res.json().then(data => {
+                if (data != null) {
+                    setArticles(data.data.children);
+                    console.log(data);
+                }
+            })
+
+        })
+    }, [subreddit]);
+
+    return ( < div className = "App" >
+        <header className = "App-header">
+        <input type = "text" className = "input" value = {subreddit} onChange={e => setSubreddit(e.target.value)} />
+        </header> <div className = "articles" >
+            {
+            (Article != null) ? articles.map((article, index) =><Article key={index} article={article.data}/>) : ""
+            }
+        </div>
+        </div >
+    );
+}
+
+export default App;
